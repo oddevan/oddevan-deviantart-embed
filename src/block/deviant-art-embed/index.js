fcf136b3598ee1eff847e318a4598333f4fe8f01
Evan Hildreth <evan.hildreth@webdevstudios.com>
2020-07-04 20:38:20 -0400

    Add images from DA

--- src/block/deviant-art-embed/index.js
+++ src/block/deviant-art-embed/index.js
@@ -3,13 +3,14 @@
  */
 import edit from './edit';
 import save from './save';
+import DaLogo from './DaLogo';
 
 import { __ } from '@wordpress/i18n';
 import { registerBlockType } from '@wordpress/blocks';
 
 registerBlockType( 'oddevan/deviant-art-embed', {
 	title: __( 'Deviant Art', 'deviant-art-embed' ),
-	icon: 'edit',
+	icon: DaLogo,
 	category: 'embed',
 	keywords: [
 		__( 'oddEvan', 'deviant-art-embed' ),
